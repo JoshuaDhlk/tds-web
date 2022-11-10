c51eb2cba153d59bbb8c8e5a43ef916f72bd76ba
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-11-10 16:13:38 +0100

    C:\xampp\htdocs\tds-web\coursphp\.php

--- coursphp/.php/connexion.php
+++ coursphp/.php/connexion.php
@@ -1,15 +1,11 @@
-<html>
-<head>
-<title>Formulaire d'identification</title>
-</head>
-
-<body>
-<form action="login.php" method="post">
-Votre login : <input type="text" name="login">
-<br />
-Votre mot de passé : <input type="password" name="pwd"><br />
-<input type="submit" value="Connexion">
+<?php //definir le doc comme php en entrant dans la session serveur//
+session_start(); //Débute la session ou la reprendre pour identifier le client//
+?>
+<form method="post"> <!--formulaire-->
+    <input type="text" name="username">
 </form>
-
-</body>
-</html>
+<?php
+if(isset($_POST['username'])) { // $_POST pour accéder à 'username dans ce cas là(peut être changé au choix)//
+    $_SESSION['user']=$_POST['username'];
+    var_dump($_SESSION); //var_dumb pour afficher la session//
+}
