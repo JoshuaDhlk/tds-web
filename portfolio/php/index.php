817709452c8a7e0ca75759ced2adf81168d8214d
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-11-17 16:58:32 +0100

    portfolioV1 & refactoring

--- portfolio/php/index.php
+++ portfolio/php/index.php
@@ -7,8 +7,20 @@
     <title>PortFolio</title>
 </head>
 <body>
+    <p>Hello World!!</p>
 <?php
-$echo "Hello world!"
+echo 'Hello World!';
+
+$filename='tableau.data';
+if(file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $array = unserialize($data);
+    var_dump($array);
+}
+$COMPETENCES = array("1","2","3");
+var_dump($COMPETENCES );
+
 ?>
+
 </body>
 </html>
