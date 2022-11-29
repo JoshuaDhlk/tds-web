93962edf6941c850b27139999d4484f7294e564d
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-11-29 19:06:46 +0100

    Add files via upload

---
+++ Projets cours/Portfolio/php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PortFolio</title>
+</head>
+<body>
+    
+<?php
+echo 'Hello World!';
+
+$filename='tableau.data';
+if(file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $array = unserialize($data);
+    var_dump($array);
+}
+$COMPETENCES = array("1","2","3");
+var_dump($COMPETENCES );
+
+?>
+
+</body>
+</html>
