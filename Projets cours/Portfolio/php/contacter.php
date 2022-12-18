56a499dc59dbf0b5191e95cc1a6ff6e7737374ba
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-12-18 20:06:23 +0100

    Add files via upload

--- Projets cours/Portfolio/php/contacter.php
+++ Projets cours/Portfolio/php/contacter.php
@@ -25,5 +25,47 @@
             <h2>7 allée de marronniers 27260</h2>
           </div>
         </div>
+        <form action="contact.php" method="post">
+          <label for="name">Nom:</label><br>
+          <input type="text" id="name" name="name"><br>
+          <label for="email">Email:</label><br>
+          <input type="text" id="email" name="email"><br>
+          <label for="subject">Sujet:</label><br>
+          <input type="text" id="subject" name="subject"><br>
+          <label for="message">Message:</label><br>
+          <textarea id="message" name="message"></textarea><br>
+          <input type="submit" value="Envoyer">
+        </form> 
+        <?php
+
+// Récupération des données du formulaire
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Configuration de l'adresse e-mail de réception
+$to = 'contact@example.com';
+$subject = 'Nouveau message de '.$name;
+
+// Contenu du message (en HTML)
+$message = '
+<html>
+<head>
+  <title>'.$subject.'</title>
+</head>
+<body>
+  <p>Vous avez reçu un nouveau message de '.$name.'</p>
+  <p><strong>Email:</strong> '.$email.'</p>
+  <p><strong>Sujet:</strong> '.$subject.'</p>
+  <p><strong>Message:</strong> '.$message.'</p>
+</body>
+</html>
+';
+
+// En-têtes de l'e-mail
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=utf-8';
+$headers[] = 'To: '.$
       </div>
     </div>
