594906d91f2913f116bbc793fa9c8053cbf92dc7
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-12-18 22:48:50 +0100

    MAJ_18/12/2022_full

---
+++ portfolio/php/home.php
@@ -0,0 +1,8 @@
+<div class="home container">
+      <div>
+        <h1>Bonjour, <span></span></h1>
+        <h1>Je m'appelle Joshua <span></span></h1>
+        <h1>Dahlke <span></span></h1>
+        <a href="#projects" type="button" class="clickable">Portfolio</a>
+      </div>
+</div>
