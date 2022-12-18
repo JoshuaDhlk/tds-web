16433e8d36b14651a70da5d8d15d3a117873fa08
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-12-18 16:26:26 +0100

    MAJ 1.1 18/12/22

---
+++ portfolio/php/nav.php
@@ -0,0 +1,25 @@
+<!-- Header -->
+<section id="header">
+<div class="header container">
+<div class="nav-bar">
+<div class="brand">
+<a href="#home">
+<h1><span>Jo</span>shua <span>D</span>ahlke</h1>
+</a>
+</div>
+<div class="nav-list">
+<div class="hamburger">
+<div class="bar"></div>
+</div>
+<ul>
+<li><a href="#home" data-after="Home">Home</a></li>
+<li><a href="#services" data-after="Service">Compétences</a></li>
+<li><a href="#projects" data-after="Projects">Projets</a></li>
+<li><a href="#about" data-after="About">A propos</a></li>
+<li><a href="#contact" data-after="Contact">Contacter</a></li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+<!-- End Header -->
