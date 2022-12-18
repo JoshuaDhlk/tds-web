12d46c3b84034d241f57a16220ecbb78c8a43e17
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-12-18 20:59:42 +0100

    Add files via upload

--- Projets cours/Portfolio/php/contacter.php
+++ Projets cours/Portfolio/php/contacter.php
@@ -9,44 +9,56 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="contact container">
-        <div>
-          <h1 class="section-title">Info <span>contact</span></h1>
-        </div>
-          <p>N'hésitez pas à me contacter.</p>
-          <form action="mail.php" method="POST">
-              <label for="name">Name:</label>
-              <input type="text" name="name" id="name">
-              <label for="email">Email:</label>
-              <input type="email" name="email" id="email">
-              <label for="subject">Subject:</label>
-              <input type="text" name="subject" id="subject">
-              <label for="message">Message</label>
-              <textarea name="message" cols="30" rows="10"></textarea>
-              <input type="submit" value="Send">
-          </form>
-  </div>
-<?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+    <div class="container">
+        <h1>Contact Me</h1>
+        <p>Feel free to contact us and we will get back to you ass sson as we can.</p>
+        <form action="mail.php" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+            <label for="subject">Subject:</label>
+            <input type="text" name="subject" id="subject">
+            <label for="message">Message</label>
+            <textarea name="message" cols="30" rows="10"></textarea>
+            <input type="submit" value="Send">
+        </form>
+    </div>
+    <?php
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $subject = $_POST['subject'];
+      $message = $_POST['message'];
 
-$mailheader = "From:".$name."<".$email.">\r\n";
+      $mailheader = "From:".$name."<".$email.">\r\n";
 
-$recipient = "warmdarkrai@gmail.com";
+      $recipient = "example@gmail.com";
 
-mail($recipient, $subject, $message, $mailheader) or die("Error!");
+      mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
-echo'
-        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
-';
+      echo'
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Contact form</title>
+          <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+          <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+          <div class="container">
+              <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+              <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+              
+          </div>
+      </body>
+      </html>
+      ';
 
-?>
+
+    ?>
 </body>
 </html>
-
-<?php
-
 
