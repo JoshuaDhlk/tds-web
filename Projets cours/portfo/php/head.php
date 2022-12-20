c43babd76670fcbbc86c15a82a8c042d5b3de515
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-12-20 18:36:14 +0100

    Delete Projets cours directory

--- Projets cours/portfo/php/head.php
+++
@@ -1,3 +0,0 @@
-<meta charset="UTF-8">
-<link rel="stylesheet" href="css/style.css" media="screen" />
-<meta name="viewport" content="width=1100">
--- Projets cours/portfoliov19/php/competences.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $data=yaml_parse_file("./yaml/competences.yaml");
-    foreach($data AS $domaine => $tab){
-        echo '<h1>'.$domaine.'</h1>';
-        foreach($tab AS $nom => $niveau){
-            echo '<p>'.$nom.' : '.$niveau.'</p>';
-        }
-    }
-?>
