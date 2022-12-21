9d87c71eb6a32d11a7316998c8670d402fdf4ca3
JoshuaDhlk <113612960+JoshuaDhlk@users.noreply.github.com>
2022-12-21 14:44:28 +0100

    MAJ_21/12/2022

---
+++ portfolio/contacter.php
@@ -0,0 +1,18 @@
+<?php 
+
+$to = 'joshuadlk.dhlk@gmail.com';
+$subject = 'Mail Portfolio';
+$name = htmlspecialchars($_POST['nom']);
+$headers = 'From:' . htmlspecialchars($_POST['mail']) . '\r\n' ;
+$message = htmlspecialchars($_POST['message']);
+
+
+
+
+    if (!empty($_POST['mail']) and !empty($_POST['nom']) and !empty($_POST['message'])){
+        mail($to, $subject, $message, $headers);
+        echo 'envoyé';
+    }
+    else 
+        echo 'erreur dans l\'envoie';
+
